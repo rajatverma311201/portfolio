73665cbc26cd4cc4ce1253ee46cd5c6fde3ee803
Rajat Verma <rajat311201@gmail.com>
2023-08-14 23:33:09 +0530

    toaster font

--- src/components/ToastComponent.jsx
+++ src/components/ToastComponent.jsx
@@ -9,13 +9,13 @@ function ToastComponent() {
             containerStyle={{ margin: "8px" }}
             toastOptions={{
                 success: {
-                    duration: 3000,
+                    duration: 5000,
                 },
                 error: {
                     duration: 5000,
                 },
                 style: {
-                    fontSize: "18px",
+                    fontSize: "1.75rem",
                     maxWidth: "500px",
                     padding: "16px 24px",
                     backgroundColor: "#222",
